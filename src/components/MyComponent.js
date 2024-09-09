9a5a53e81144b06d4e82b768f02f0b2b71254682
MyTLTT <myle10032001@gmail.com>
2024-09-09 21:17:07 +0700

    [feat] class component

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,11 +1,19 @@
 import React from "react";
 
 class MyComponent extends React.Component {
-    // JSX
+    state = {
+        name: 'my',
+        address: 'Tam ky',
+        age: 21
+    }
+
+    //jsx
     render() {
         return (
-            <div> my first component {Math.random()}</div>
-        );
+            <div>
+                My name is {this.state.name} adress {this.state.address}
+            </div>
+        )
     }
 }
 
