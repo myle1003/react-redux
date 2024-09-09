d3e23f9e9b278ab3605c26bcbcd5d899ea4a79b5
MyTLTT <myle10032001@gmail.com>
2024-09-09 21:57:33 +0700

    [feat] arrow function

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -7,11 +7,30 @@ class MyComponent extends React.Component {
         age: 21
     }
 
+    handleClick = (event) => {
+        console.log(event.taget)
+        console.log(`My name is`, this.state.name);
+
+        this.setState({
+            name: 'selina',
+            age: Math.floor((Math.random() * 100) + 1)
+        })
+
+    }
+
+    handleOnMouseOver(event) {
+        // console.log(event.taget)
+        // console.log(`Iam from`, this.state.address);
+    }
+
+
     //jsx
     render() {
         return (
             <div>
-                My name is {this.state.name} adress {this.state.address}
+                My name is {this.state.name} adress {this.state.age}
+                <button onClick={this.handleClick}>Click me</button>
+                <button onMouseOver={(event) => { this.handleOnMouseOver(event) }}>Hover me</button>
             </div>
         )
     }
