7935620e82556af13fbbd80f0c6a5a500c222505
MyTLTT <myle10032001@gmail.com>
2024-09-18 23:13:36 +0700

    [feat] add header

--- src/App.js
+++ src/App.js
@@ -1,39 +1,10 @@
 import './App.scss';
-
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <div className='app-container'>
-//         helo tra & me
-//         <MyComponent></MyComponent>
-//       </div>
-//     )
-//   }
-
-// }
-
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Hello My ne
-//         </p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//   );
-// }
+import Header from './components/Header/Header';
 
 const App = () => {
   return (
-    <div className="App-containt" > helllo
+    <div className="App-containt" >
+      <Header />
     </div>
 
   )
