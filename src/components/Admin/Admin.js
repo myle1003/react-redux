aa48fc8bc4bbab38ac6ac89cd3d05b6d1ca8b922
MyTLTT <myle10032001@gmail.com>
2024-09-21 00:50:13 +0700

    [feat] admin sidesbar

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,7 +1,19 @@
+import SideBar from "./SideBar";
+import './Admin.scss'
+import { FaBars } from "react-icons/fa";
+import { useState } from "react";
+
 const Admin = (pops) => {
+    const [collapsed, setCollapsed] = useState(false)
     return (
-        <div>
-            Admin components
+        <div className="admin-container">
+            <div className="admin-sidebar">
+                <SideBar collapsed={collapsed} />
+            </div>
+            <div className="admin-content">
+                <FaBars onClick={() => setCollapsed(!collapsed)} />
+                admin
+            </div>
         </div>
     )
 }
