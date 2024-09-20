0b95a64adf00c2c8cc3bdf8de13967686d72f448
MyTLTT <myle10032001@gmail.com>
2024-09-21 01:13:42 +0700

    [feat] add lib

--- src/components/Admin/SideBar.js
+++ src/components/Admin/SideBar.js
@@ -8,10 +8,12 @@ import {
     SidebarFooter,
     SidebarContent,
 } from 'react-pro-sidebar';
+import './SideBar.scss';
 
 import { FaTachometerAlt, FaGem, FaList, FaGithub, FaRegLaughWink, FaHeart } from 'react-icons/fa';
 import sidebarBg from '../../assets/bg2.jpg';
-
+import { DiReact } from "react-icons/di";
+import { MdDashboard } from "react-icons/md";
 
 const SideBar = (props) => {
     const { image, collapsed, toggled, handleToggleSidebar } = props;
@@ -37,6 +39,7 @@ const SideBar = (props) => {
                             whiteSpace: 'nowrap',
                         }}
                     >
+                        <DiReact size={"3em"} color={"00bfff"} />
                         Hoi Dan IT
                     </div>
                 </SidebarHeader>
@@ -44,21 +47,20 @@ const SideBar = (props) => {
                 <SidebarContent>
                     <Menu iconShape="circle">
                         <MenuItem
-                            icon={<FaTachometerAlt />}
-                            suffix={<span className="badge red">New</span>}
+                            icon={<MdDashboard />}
                         >
-                            dashboard
+                            Dashboard
                         </MenuItem>
-                        <MenuItem icon={<FaGem />}> components </MenuItem>
                     </Menu>
                     <Menu iconShape="circle">
                         <SubMenu
-                            suffix={<span className="badge yellow">3</span>}
-                            icon={<FaRegLaughWink />}
+                            icon={<FaGem />}
+                            title="Features"
+
                         >
-                            <MenuItem> 1</MenuItem>
-                            <MenuItem> 2</MenuItem>
-                            <MenuItem> 3</MenuItem>
+                            <MenuItem> Quan ly user</MenuItem>
+                            <MenuItem> Quan ly bai quiz</MenuItem>
+                            <MenuItem> QUan ly cau hoi</MenuItem>
                         </SubMenu>
 
                     </Menu>
@@ -79,7 +81,7 @@ const SideBar = (props) => {
                         >
                             <FaGithub />
                             <span style={{ whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden' }}>
-                                viewSource
+                                Hoi me
                             </span>
                         </a>
                     </div>
