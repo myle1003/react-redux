99ebbfcf94bbe46a7d2f710e3a368f41a4d388a8
MyTLTT <myle10032001@gmail.com>
2024-09-22 22:11:07 +0700

    [feat] validate data

--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { FcPlus } from 'react-icons/fc'
+import { toast } from 'react-toastify';
 
 const ModalCreateUser = (props) => {
     const { show, setShow } = props;
@@ -33,15 +34,24 @@ const ModalCreateUser = (props) => {
         }
     }
 
+    const validateEmail = (email) => {
+        return String(email)
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+    };
+
     const handleSubmitCreateUser = async () => {
-        // let data = {
-        //     email: email,
-        //     password: password,
-        //     username: username,
-        //     role: role,
-        //     userImage: image,
-        // }
-        // console.log(data);
+        const isValisateEmain = validateEmail(email);
+        if (!isValisateEmain) {
+            toast.error(`Invalid email address`);
+            return;
+        }
+        if (!password) {
+            toast.error(`Password is required`);
+            return;
+        }
         const data = new FormData();
         data.append('email', email);
         data.append('password', password);
@@ -49,8 +59,15 @@ const ModalCreateUser = (props) => {
         data.append('role', role);
         data.append('userImage', image);
         let res = await axios.post('http://localhost:8081/api/v1/participant', data);
-        console.log("Success", res);
 
+        if (res.data && res.data.EC === 0) {
+            toast.success(res.data.EM)
+            handleClose();
+            return;
+        }
+
+        toast.error(res.data.EM);
+        return;
 
     }
 
